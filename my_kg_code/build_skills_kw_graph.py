c26a58c793adafc8eb766f63fef9cc2c00737c4e
shalder <shalder@users.noreply.github.com>
2018-07-30 13:31:23 +0530

    Create build_skills_kw_graph.py

---
+++ my_kg_code/build_skills_kw_graph.py
@@ -0,0 +1,96 @@
+import sys
+import traceback
+
+from gremlin_python import statics
+from gremlin_python.structure.graph import Graph
+from gremlin_python.process.graph_traversal import __
+from gremlin_python.process.strategies import *
+from gremlin_python.process.traversal import T
+from gremlin_python.process.traversal import Order
+from gremlin_python.process.traversal import Cardinality
+from gremlin_python.process.traversal import Column
+from gremlin_python.process.traversal import Direction
+from gremlin_python.process.traversal import Operator
+from gremlin_python.process.traversal import P
+from gremlin_python.process.traversal import Pop
+from gremlin_python.process.traversal import Scope
+from gremlin_python.process.traversal import Barrier
+from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+statics.load_statics(globals())
+graph = Graph()
+
+try:
+    # 8182 is the default gremlin Server port
+    g = graph.traversal().withRemote(DriverRemoteConnection('ws://localhost:8182/gremlin','g'))
+except Exception as e:
+    print ('Connection to gremlin server failed. Connection error stack trace:')
+    print (traceback.format_exc())
+    sys.exit(-1)
+
+# g.V().drop().iterate()
+# g.E().drop().iterate()
+# print (g.V().count().next())
+
+import csv
+
+# The skill domain and skill vertices are stores in a file which needs to be loaded into the 
+# Graph DB to form the Knowledge Graph
+try:
+    skillreader = csv.reader(open('skill_vertex.csv'), delimiter=',')
+except Exception as e:
+    print ('Failed to load Vertex CSV File "skill_vertex.csv".')
+    exit(-1)
+
+# Remove any stray whitespace characters when reading the skill names from the CSV files
+
+skillreader = [(row[0].strip(), row[1].strip()) for row in skillreader]
+for row in skillreader:
+    
+    # See if the skill node is present already
+    count = g.V().has('name', row[1]).count().next()
+    # print (count > 1)
+    if count == 0:
+        # print ('Adding vertex ' + row[1])
+        g.addV(row[0]).property('name', row[1]).next()
+
+    if count > 0:
+        print ('Node already present with count: ' + row[1] + ' ' + str(count))
+
+print ("*" * 76)
+print (" " * 25 + "Vertex loading complete")
+print ("*" * 76)
+
+# The relationships between nodes are stored as a edge list in a CSV file where the
+# first column has the originating node name and the second column has the destination
+# node name 
+try:
+    skillreader = csv.reader(open('skill_edges.csv'), delimiter=',')
+except Exception as e:
+    print ('Failed to load Vertex CSV File "skill_edges.csv".')
+    exit(-1)
+
+skillreader = [(row[0].strip(), row[1].strip()) for row in skillreader]
+
+for row in skillreader:
+    # See if the node is present already
+    for idx in [0,1]:
+        count = g.V().has('name', row[idx]).count().next()
+        
+        if count != 1:
+
+            # Ensure there are no duplicate skills before adding an edge between them
+            print ('Duplicate vertex found: ' + row[idx])
+            sys.exit(-1)
+
+    left = g.V().has('name', row[0]).next().id
+    right = g.V().has('name', row[1]).next().id
+    count = g.V(left).out('related').hasId(right).count().next()
+    if count == 0:
+        print ('Adding edge from ' + row[0] + " to " + row[1])
+        g.V(right).as_('r').V(left).addE('related').to('r').toList()
+
+
+print ()
+print ("*" * 76)
+print (" " * 25 + "Edge creation complete")
+print ("*" * 76)
